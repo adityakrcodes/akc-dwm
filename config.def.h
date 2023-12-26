dc2e2baa8441025e2ef15fd99814f6a62dc1d38a
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-12-27 02:32:57 +0530

    UPDATE: Changed terminal to kitty

--- config.def.h
+++ config.def.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "wezterm", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *rofi[] = { "rofi","-show", "drun", NULL };
 static const char *dolphin[] = { "dolphin", NULL};
 static const char *browser[] = { "firefox", NULL};
