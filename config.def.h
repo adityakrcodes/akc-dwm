93738573fbd97ddf8ed624fa0eb014825d87ab82
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-09-30 04:22:56 +0530

    UPDATE: Changed the terminal keybing to Warp Terminal

--- config.def.h
+++ config.def.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "warp-terminal", NULL };
 static const char *rofi[] = { "rofi","-show", "drun", NULL };
 static const char *filemgr[] = { "nautilus", NULL};
 static const char *browser[] = { "brave", NULL};
